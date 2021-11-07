0ded12170c6e645d61805cb55a7c67930710ceb7
Jack Thompson <github@jackthompson.me.uk>
2021-11-07 23:32:07 +0000

    re-design

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1.6rem',
+                sm: '4rem',
+                lg: '6rem',
+                xl: '10rem',
+            },
+        },
+        fontFamily: {
+            'sans': ['Urbanist', 'sans-serif'],
+        },
+        extend: {
+            fontSize: {
+                '2.5xl': '1.625rem',
+                '3.5xl': '2rem'
+            },
+            letterSpacing: {
+                'massive': '0.2em'
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('dist');
-
-mix.js('src/js/app.js', 'dist/').sass('src/scss/app.scss', 'dist/');
-
-mix.webpackConfig({ node: { fs: 'empty', net: 'empty', tls: 'empty' }})
